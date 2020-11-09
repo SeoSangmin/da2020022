2a73846b2213ef9d15c2c72b89ae1550c3d80061
SangMin <seoinseoulcity@gmail.com>
2020-11-09 15:18:55 +0900

    Update firstA.java

--- TermProject/src/firstA.java
+++ TermProject/src/firstA.java
@@ -9,14 +9,11 @@ public class firstA {
 	 * 
 	 * 2. Test1 : 객체를 구성하지 않고 하나의 파일에 코드를 짰습니다.
 	 * 
-	 * 3. 두 가지 방식 모두 랜덤으로 생성되는 데이터가 너무 범위가 큰 것을 원치 않아
-	 *    bound를 100으로 설정해놓았습니다.
-	 * 
 	 * + git에 처음 올린 파일에는 1. 방법에서 클래스 객체가 분리되어있지 않습니다.
 	 *   또 올바르게 동작하는지 보기 위해 배열에 각 원소들을 출력하는 코드를 작성한 것을
 	 *   삭제하지 않았습니다.
-	 *   
-	 *   현재 올리는 두 번째 파일이 첫 번째 과제 제출용입니다.
+	 * 
+	 * + 랜덤 넘버 범위가 인트 범위라고 하셔서 숫자 범위만 다시 수정했습니다.
 	 */
 	
 	public static void main(String[] args) {
@@ -32,7 +29,7 @@ public class firstA {
 		int b2 = sc.nextInt();
 		pr.array = new int[pr.size];
 		for (int i = 0; i < pr.array.length; i++) {
-	        pr.array[i] = rd.nextInt(100);
+	        pr.array[i] = rd.nextInt();
 	    }
 		sc.close();
 
